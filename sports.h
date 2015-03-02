64fffdda38406537b1be53f87aef2be47b3c3406
UmeshDhungana <dumesh77@gmail.com>
2015-03-02 14:18:09 +0545

    Update sports.h
    Updated with destructor

--- sports.h
+++ sports.h
@@ -1,7 +1,8 @@
-
 #include <iostream>
-using namespace std;
 
+#ifndef SPORTS_H
+#define SPORTS_H
+using namespace std;
 class Sports
 {
 private:
@@ -10,9 +11,12 @@ private:
 
 public:
     Sports(string playerName, string playerBest);
+    ~Sports();
     void setSportsName(string playerName);
     void setSportsBest(string playerBest);
     string getSportsName();
     string getSportsBest();
     void displaySportsInfo();
 };
+
+#endif // SPORTS_H
